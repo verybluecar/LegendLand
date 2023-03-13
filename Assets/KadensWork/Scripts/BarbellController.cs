9f45df8493531e8d3d467e1f392bdf2fcc930ade
HelloKado <choppaontop@gmail.com>
2023-03-13 09:42:27 -0700

    betterbarek
    af

--- Assets/KadensWork/Scripts/BarbellController.cs
+++ Assets/KadensWork/Scripts/BarbellController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using System.Collections;
 
@@ -5,37 +6,8 @@ public class BarbellController : MonoBehaviour
 {
     public float pickupRange = 2f;
     public Transform pickupDestination;
-    public float moveSpeed = 1f;
-    public float moveRange = 1f;
     private bool isHoldingBarbell = false;
     private Transform heldBarbell;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (isHoldingBarbell)
-            {
-                // Start moving the barbell up and down
-                StartCoroutine(MoveBarbell());
-            }
-        }
-    }
-
-    private IEnumerator MoveBarbell()
-    {
-        float startY = heldBarbell.position.y;
-        float endY = startY + moveRange;
-        float progress = 0f;
-
-        while (progress < 1f)
-        {
-            progress += moveSpeed * Time.deltaTime;
-            float newY = Mathf.Lerp(startY, endY, Mathf.PingPong(progress, 1f));
-            heldBarbell.position = new Vector3(heldBarbell.position.x, newY, heldBarbell.position.z);
-            yield return null;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -49,15 +21,5 @@ public class BarbellController : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (isHoldingBarbell && other.transform == heldBarbell)
-        {
-            heldBarbell.parent = null;
-            heldBarbell.GetComponent<Rigidbody>().isKinematic = false;
-            heldBarbell = null;
-            isHoldingBarbell = false;
-        }
-    }
 }
 
